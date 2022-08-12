0524f41113ca66bb327016176b7889b633395e95
Steven Moore <steven.moore177@gmail.com>
2022-08-12 16:59:35 +0200

    push

--- fdd/settings.py
+++ fdd/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-dybas%2^kbmx!5q(!g$e^m@v3*0npo%b7b=ji%i*ed=4c*btr('
+SECRET_KEY = 'django-insecure-dybas%2^kbmx!5q(!g$e^m@v3*0npo%b7b=ji%i*ed=4c*btr('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
